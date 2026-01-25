e708d1853bb9c3792337f53d81709afae3ab7548
sagit101 <saadproject49@gmail.com>
2026-01-25 15:15:01 -0600

    mnt

--- all modules/blobmodule/outputs.tf
+++ all modules/blobmodule/outputs.tf
@@ -2,12 +2,12 @@
 # Storage account name
 ############################################
 output "yellow_storage_account_name" {
-  value = azurerm_storage_account.yellow_sa.name
+  value = azurerm_storage_account.tf_yellow_sa.name
 }
 
 ############################################
 # Container name
 ############################################
 output "yellow_container_name" {
-  value = azurerm_storage_container.yellow_container.name
+  value = azurerm_storage_container.tf_yellow_container.name
 }
