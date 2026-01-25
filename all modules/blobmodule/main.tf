2e09b681642e0cfddc5c5230b2d9918039ce4476
sagit101 <saadproject49@gmail.com>
2026-01-25 02:28:36 -0600

    mnt

--- all modules/blobmodule/main.tf
+++ all modules/blobmodule/main.tf
@@ -1,35 +1,29 @@
 ############################################
 # Resource Group
-# Dedicated RG for storage stack
 ############################################
 resource "azurerm_resource_group" "yellow_rg" {
   name     = "yellow-storage-rg"
-  location = var.location # blue
+  location = var.location
 }
 
 ############################################
 # Storage Account
-# Cheapest general-purpose storage
+# Standard / LRS = cheapest
 ############################################
 resource "azurerm_storage_account" "yellow_sa" {
-  name                     = var.storage_account_name # blue (must be globally unique, lowercase)
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.yellow_rg.name
   location                 = azurerm_resource_group.yellow_rg.location
 
-  # Cheapest standard storage
   account_tier             = "Standard"
-  account_replication_type = "LRS"   # cheapest replication
-
-  # Required settings
-  allow_blob_public_access = false
+  account_replication_type = "LRS"
 }
 
 ############################################
 # Blob Container
-# Logical container inside the storage account
 ############################################
 resource "azurerm_storage_container" "yellow_container" {
-  name                  = var.container_name # blue
+  name                  = var.container_name
   storage_account_name  = azurerm_storage_account.yellow_sa.name
   container_access_type = "private"
 }
